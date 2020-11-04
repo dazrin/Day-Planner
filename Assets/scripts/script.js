561ebe326dc5c17d798f684c204cde828230d4d2
Dazrin Tioseco <xdazee@gmail.com>
2020-11-04 00:18:36 -0800

    Finished functionality and style

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 
     //Save Button; When clicked, stores both text and time to local storage
     $(".saveBtn").on("click", function () {
-        var text = $(this).siblings(".decription").val();
+        var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
 
         localStorage.setItem(time, text);
@@ -21,6 +21,8 @@ $(document).ready(function () {
     $("#hr16 .description").val(localStorage.getItem("hr16"));
     $("#hr17 .description").val(localStorage.getItem("hr17"));
 
+
+
     function trackHour() {
         //Current time
         var currentHr = moment().hour();
@@ -28,13 +30,14 @@ $(document).ready(function () {
         //Store the hour of each time block as a number in a variable
         $(".time-block").each(function () {
             var hrBlock = parseInt($(this).attr("id").split("hour")[1]);
+            console.log(hrBlock, currentHr)
         
         //If the current time has moved past the hour in the block, 
         //Set the "past" class on that block and remove any other classes
-            if(hrBlock < currentHr) {
+            if (hrBlock < currentHr) {
                 $(this).addClass("past");
-                $(this).removeClass("present");
                 $(this).removeClass("future");
+                $(this).removeClass("present");
 
         //If the current time is equal to the time on the block
         //Set the "present" class on that block and remove any other classes
@@ -45,11 +48,11 @@ $(document).ready(function () {
 
         //Otherwise, remove any other classes and add the "future" class to the other block        
             } else {
-                $(this).removeClass("past");
                 $(this).removeClass("present");
+                $(this).removeClass("past");
                 $(this).addClass("future");
             }
-        });
+        })
     }
     trackHour();
 });
